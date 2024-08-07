353f85720fb9e9719757ce7927620d0453238960
Gino <ginowine1@gmail.com>
2024-08-07 12:14:10 +0100

    feat(devx): create token and update pool contract

--- contracts/YieldFarm.sol
+++ contracts/YieldFarm.sol
@@ -2,11 +2,108 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract YieldFarm{
+contract YieldFarm is Ownable{
+
+    string public name = "Reward Token Farm";
+    IERC20 public rewardToken;
+
     address[] public stakers;
     // token > address
     mapping(address => mapping(address => uint256)) public stakingBalance;
     mapping(address => uint256) public uniqueTokensStaked;
     mapping(address => address) public tokenPriceFeedMapping;
+
+    address[] allowedTokens;
+
+    address[] allowedTokens;
+
+    constructor(address _rewardTokenAddress) public {
+        rewardToken = IERC20(_rewardTokenAddress);
+    }
+
+    function addAllowedTokens(address token) public onlyOwner {
+        allowedTokens.push(token);
+    }
+
+    function setPriceFeedContract(address token, address priceFeed)
+        public
+        onlyOwner
+    {
+        tokenPriceFeedMapping[token] = priceFeed;
+    }
+
+    function stakeTokens(uint256 _amount, address token) public {
+
+        require(_amount > 0, "amount should be greater than zero");
+        if(tokenIsAllowed(token)){
+            updateUniqueTokensStaked(msg.sender, token);
+            IERC20(token).transferFrom(msg.sender, address(this), _amount);
+            stakingBalance[token][msg.sender] = stakingBalance[token][msg.sender] + _amount;
+            if(uniqueTokensStaked[msg.sender] == 1){
+                stakers.push(msg.sender);
+            }
+        }
+    }
+
+    // Unstaking Tokens (Withdraw)
+    function unstakeTokens(address token) public {
+        // Fetch staking balance
+        uint256 balance = stakingBalance[token][msg.sender];
+        require(balance > 0, "staking balance cannot be 0");
+        IERC20(token).transfer(msg.sender, balance);
+        stakingBalance[token][msg.sender] = 0;
+        uniqueTokensStaked[msg.sender] = uniqueTokensStaked[msg.sender] - 1;
+    }
+
+    function getUserTotalValue(address user) public view returns (uint256) {
+        uint256 totalValue = 0;
+        if (uniqueTokensStaked[user] > 0) {
+            for (
+                uint256 allowedTokensIndex = 0;
+                allowedTokensIndex < allowedTokens.length;
+                allowedTokensIndex++
+            ) {
+                totalValue =
+                    totalValue +
+                    getUserStakingBalanceEthValue(
+                        user,
+                        allowedTokens[allowedTokensIndex]
+                    );
+            }
+        }
+        return totalValue;
+    }
+
+    function tokenIsAllowed(address token) public returns (bool) {
+        for (
+            uint256 allowedTokensIndex = 0;
+            allowedTokensIndex < allowedTokens.length;
+            allowedTokensIndex++
+        ) {
+            if (allowedTokens[allowedTokensIndex] == token) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function updateUniqueTokensStaked(address user, address token) internal {
+        if (stakingBalance[token][user] <= 0) {
+            uniqueTokensStaked[user] = uniqueTokensStaked[user] + 1;
+        }
+    }
+
+    function getUserStakingBalanceEthValue(address user, address token)
+        public
+        view
+        returns (uint256)
+    {
+        if (uniqueTokensStaked[user] <= 0) {
+            return 0;
+        }
+        return
+            (stakingBalance[token][user] * getTokenEthPrice(token)) / (10**18);
+    }
 }
